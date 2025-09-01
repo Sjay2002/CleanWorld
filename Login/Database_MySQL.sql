1aa62236e44624985924a8168692f34c362cda9e
Savidya Sithumini Jayasekara <141916226+Sjay2002@users.noreply.github.com>
2025-09-02 00:07:41 +0530

    Delete Login directory

--- Login/Database_MySQL.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
-
-INSERT INTO users (username, password) 
-VALUES ('admin', 'password123'), ('user1', 'mypassword');
