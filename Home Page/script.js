a5776afb6adb8b72dd459b68b2b204d901d8b7c3
Savidya Sithumini Jayasekara <141916226+Sjay2002@users.noreply.github.com>
2025-09-02 00:07:30 +0530

    Delete Home Page directory

--- Home Page/script.js
+++
@@ -1,8 +0,0 @@
-const featureBoxes = document.querySelectorAll('.feature-box');
-
-featureBoxes.forEach(box => {
-    box.addEventListener('click', function() {
-        const url = this.getAttribute('data-url');
-        window.location.href = url;
-    });
-});
